79dc060660ef6d316d81ca57033b85763e956152
rpolacheck <rpolacheck@gmail.com>
2019-10-17 17:35:24 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageViewComponent } from './components/page-view/page-view.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageViewComponent,
+    ContactFormComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
