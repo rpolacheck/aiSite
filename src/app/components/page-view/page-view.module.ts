d12b962c7b539e968427f65f484f4aa050c6808e
rpolacheck <rpolacheck@gmail.com>
2019-10-18 09:19:44 -0500

    Add modules, angular material and themes

--- src/app/components/page-view/page-view.module.ts
+++ src/app/components/page-view/page-view.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+import { PageViewComponent } from './page-view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageViewComponent],
   imports: [
+    BrowserAnimationsModule,
     CommonModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule
   ]
 })
 export class PageViewModule { }
